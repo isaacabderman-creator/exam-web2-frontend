6b99515e39dea50e5ec5a7148b935317015994af
ANDRIANARIMANANA Isaac Abderman <isaac@mail.hei.school>
2026-08-23 08:49:52 +0300

    ref: remove unnecessary comment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import "./Navbar.css";   // ← c'était l'import manquant, cause du bug visuel
+import "./Navbar.css"; 
 
 export default function Navbar() {
   const { user, logout } = useAuth();
