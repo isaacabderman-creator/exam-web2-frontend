f8417a1272c9bffe4283e62007a3fe134c81c6d9
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-08-27 12:45:42 +0300

    feat(route): wire admin exams pages into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Login from "./pages/Login/Login.jsx";
 import Students from "./pages/admin/Students/Students.jsx";
 import Dashboard from "./pages/admin/Dashboard/Dashboard.jsx";
 import Courses from "./pages/admin/Courses/Courses.jsx";
+import Exams from "./pages/admin/Exams/Exams.jsx";
+import ExamForm from "./pages/admin/Exams/ExamForm.jsx";
+import ExamQuestions from "./pages/admin/Exams/ExamQuestions.jsx";
+import ExamResults from "./pages/admin/Exams/ExamResults.jsx";
 import AvailableExams from "./pages/student/AvailableExams/AvailableExams.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
@@ -17,6 +21,11 @@ function Layout() {
           <Route path="/admin" element={<Dashboard />} />
           <Route path="/admin/students" element={<Students />} />
           <Route path="/admin/courses" element={<Courses />} />
+          <Route path="/admin/exams" element={<Exams />} />
+          <Route path="/admin/exams/new" element={<ExamForm />} />
+          <Route path="/admin/exams/:id/edit" element={<ExamForm />} />
+          <Route path="/admin/exams/:id/questions" element={<ExamQuestions />} />
+          <Route path="/admin/exams/:id/results" element={<ExamResults />} />
           <Route path="/student/exams" element={<AvailableExams />} />
       </Routes>
     </>
